68d55ab5761962b3d93c7747686c09c7243e579f
YooJini <wlsdl6685@gmail.com>
2020-06-08 13:57:37 +0900

    알파버전 모델 교체  1) 백그라운드 스크롤링  2) 캐릭터 모델 교체 (플레이어, 에너미, 보스)
      - 유니티버전: Unity 2019.3.13f1(64-bit)
      -  비주얼스튜디오: 2019 community

---
+++ Unity Project/Assets/_JINI/Scripts/BackGround.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    Material mat;
+    public float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //머티리얼은 렌더러 컴포넌트안에 속성으로 붙어있다.
+        mat = GetComponent<MeshRenderer>().material;
+        //mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BackGroundScroll();
+    }
+
+    private void BackGroundScroll()
+    {
+        //아래와 같은걸 캐스팅이라고 한다
+        //transform.position 조정할 때의 방법과 동일
+
+        //머티리얼의 메인텍스처 오프셋은 Vector2로 만들어져 있다
+        Vector2 offset = mat.mainTextureOffset;
+        //offset.y의 값만 보정해주면 된다 
+        offset.Set(0, offset.y + (speed * Time.deltaTime));
+        //다시 머티리얼 오프셋에 담는다
+        mat.mainTextureOffset = offset;
+    }
+}
